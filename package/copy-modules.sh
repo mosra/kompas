5b6c10dc7c1a66459c9e9e02bf121cc9f0cfb8a4
Vladimír Vondruš <mosra@centrum.cz>
2011-02-17 20:16:00 +0100

    Script for syncing CMake modules between repositories.

---
+++ package/copy-modules.sh
@@ -0,0 +1,9 @@
+cd ..
+cp core/modules/FindKompasCore.cmake qt/modules/FindKompasCore.cmake
+cp core/modules/FindKompasCore.cmake plugins/modules/FindKompasCore.cmake
+cp core/modules/FindKompasCore.cmake plugins-nonfree/modules/FindKompasCore.cmake
+
+cp qt/modules/FindKompasQt.cmake plugins/modules/FindKompasQt.cmake
+cp qt/modules/FindKompasQt.cmake plugins-nonfree/modules/FindKompasQt.cmake
+
+cp plugins/modules/FindKompasPlugins.cmake plugins-nonfree/modules/FindKompasPlugins.cmake
