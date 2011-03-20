516c09a4efabf0afeaa47583da6ff101097c6a2d
Vladimír Vondruš <mosra@centrum.cz>
2011-03-20 22:11:13 +0100

    Bash script for creating changelog for release announcement.

---
+++ package/changelog.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+prev=v0.1.1
+curr=0.1
+
+git_log() {
+    cd ../$1
+    echo -e "\n* $2:\n"
+
+    git log --oneline $prev..$curr | tee
+}
+
+echo -e "Kompas - changes from $(echo $prev | tr -d v) to $(echo $curr | tr -d v)"
+
+git_log core "Core"
+git_log plugins "Plugins"
+git_log plugins-nonfree "Non-free plugins"
+git_log qt "Qt GUI"
